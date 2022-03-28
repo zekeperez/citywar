285bc461ad123bd7db6e58f719a6c7b46063777a
Zekiel Perez <74650932+zekeperez@users.noreply.github.com>
2022-03-29 01:23:59 +0800

    Initial Commit
    Added building assets and basic mat handler

---
+++ City War/Assets/Script/BuildingMat.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingMat : MonoBehaviour
+{
+    List<Color> origColors = new List<Color>();
+    List<Material> matList = new List<Material>();
+
+    Renderer ren;
+
+    private void Awake()
+    {
+        ren = GetComponent<Renderer>();
+
+        setupMats();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            setMat("trapped");
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            setMat("captured");
+        }
+    }
+
+    public void setMat(string state)
+    {
+        switch (state.ToLower())
+        {
+            case "bombed":
+            case "destroyed":
+
+                for(int i = 0; i < matList.Count; i++)
+                {
+                    matList[i].color = Color.black;
+                }
+
+                break;
+
+            case "trapped":
+                for (int i = 0; i < matList.Count; i++)
+                {
+                    matList[i].color = Color.blue;
+                }
+                break;
+
+            case "captured":
+                for (int i = 0; i < matList.Count; i++)
+                {
+                    matList[i].color = Color.red;
+                }
+                break;
+
+            case "stronghold":
+                for (int i = 0; i < matList.Count; i++)
+                {
+                    matList[i].color = Color.yellow;
+                }
+                break;
+
+            case "original":
+            default:
+
+                for(int i = 0; i < matList.Count; i++)
+                {
+                    matList[i].color = origColors[i];
+                }
+
+                break;
+        }
+    }
+
+    #region setup
+    void setupMats()
+    {
+        int matLength = ren.materials.Length;
+
+        for(int i = 0; i < matLength; i++)
+        {
+            matList.Add(ren.materials[i]);
+        }
+
+        for(int s = 0; s < matLength; s++)
+        {
+            origColors.Add(ren.materials[s].color);
+        }
+    }
+
+    #endregion
+}
