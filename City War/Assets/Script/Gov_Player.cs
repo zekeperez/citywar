c51f58e6e369db83af0efc9eaaddede100afaf04
Zekiel Perez <74650932+zekeperez@users.noreply.github.com>
2022-04-02 17:01:07 +0800

    Added descriptions

--- City War/Assets/Script/Gov_Player.cs
+++ City War/Assets/Script/Gov_Player.cs
@@ -28,6 +28,7 @@ public class Gov_Player : MonoBehaviour
     public Image actionSprite;
     public Text actionHeader;
     public Text actionDescription;
+    string lineBreakGap = "\n\n\n";
 
     public GameObject shopConfirmation;
     public GameObject shopExplanation;
@@ -101,29 +102,77 @@ public class Gov_Player : MonoBehaviour
 
     void setActionConfirmation(int index)
     {
+        actionSprite.sprite = buttonLogos[index];
+
         switch (index)
         {
             case 0: //bomb
             default:
-                actionSprite.sprite = buttonLogos[0];
                 actionHeader.text = "Bomb";
 
-                actionDescription.text = "";
+                actionDescription.text = "The bomb is a lethal explosive device capable of fully destroying buildings.\n" +
+                                                            "When used, anyone living inside will cease to exist, including the\n" +
+                                                            "functionalities of the building." + lineBreakGap +
+
+                                                            "+ Destroying the final stronghold can net you the win. \n" +
+                                                            "= Destroying captured buildings won't have any repercussions, however \n" + 
+                                                            "the citizens might think otherwise.\n" + 
+                                                            "- Destroying innocent buildings will have repercussions.";
                 break;
 
             case 1: //drone
+                actionHeader.text = "Drone";
+
+                actionDescription.text = "Provides a surveillance drone to the target building.\n" +
+                                                            "Reveals the current status of the building." + lineBreakGap +
+
+                                                            "+ Reveals the real status of the building on that current turn.\n" + 
+                                                            "+ Citizens will gradually become unhappy due to its invasive nature.";
                 break;
 
             case 2: //shield
+                actionHeader.text = "Defense";
+
+                actionDescription.text = "Send troops to defend a building. Prevents any form of capture.\n" +
+                                                            "However, their presence will definitely displease the tenants." + lineBreakGap +
+
+                                                            "+ Prevents the Terrorist from capturing the building once.\n" +
+                                                            "- Reduce overall happiness.";
                 break;
 
             case 3: //barge
+                actionHeader.text = "Barge In";
+
+                actionDescription.text = "Send troops to barge into a building with lethal force authorized.\n" + 
+                                                            "Well - trained troops can decipher situations better than untrained troops." + lineBreakGap + 
+
+
+                                                            "+ Captured buildings will return to a neutral state.\n" + 
+                                                            "- Barging in to innocent buildings will have repercussions.\n" + 
+                                                            "-Strongholds can fend off barge in attacks.\n" + 
+                                                             "- Untrained officers may cause casualties.";
                 break;
 
             case 4: //police
+                actionHeader.text = "Patrol Check";
+
+                actionDescription.text = "Send troops to check in on houses. Depending on population happiness,\n" +
+                                                            "civilians have the right to decline. Definitely cheaper and less intrusive\n" +
+                                                            "than the drone." + lineBreakGap +
+
+                                                            "= Only reveals if the building is currently occupied or not.\n" +
+                                                            "= Chances of reveal depends on population happiness.";
                 break;
 
             case 5: //repair
+                actionHeader.text = "Building Reparations";
+
+                actionDescription.text = "Hire a repair team and rebuild the destroyed building. The building will\n" +
+                                                            "immediately be available for use." + lineBreakGap +
+
+                                                            "+ Repair a building.\n" +
+                                                            "+ Increases population happiness.\n" +
+                                                            "-It will instantly be available for use for the next player.";
                 break;
         }
     }
